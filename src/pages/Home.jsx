b4209236ca27fec02c8edd65e8ac7aac9edccfe7
Cezar <cezarrull12345@gmail.com>
2025-08-15 10:59:59 -0300

    :art: add desing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,15 @@ export function Home() {
     return(
         <>
         <div className="bodyHome">
-            
+            <div className="itemOne">
+                <div className="ballOne"></div>
+                <div className="ballSmallOne"></div>
+            </div>
+            <div className="mainHome"></div>
+            <div className="itemTwo">
+                <div className="ballTwo"></div>
+                <div className="ballSmallTwo"></div>
+            </div>
         </div>
         </>
     )
