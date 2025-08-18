0a1ef36ba7bcc2e5584b8b7b70a94fdaf567ef20
Cezar <cezarrull12345@gmail.com>
2025-08-18 02:03:10 -0300

    :art: adjustments and add new desing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,54 @@
-export function Home() {
+import { MoonStars } from '@phosphor-icons/react'
+import { SunDim } from '@phosphor-icons/react'
+import { useState } from 'react'
+
+export function Home({toggleNightMode}) {
+    const [activeNight, setActiveNight] = useState(true);
+
+    const handleButtonNight = () => {
+        setActiveNight(!activeNight);
+        toggleNightMode(!activeNight);
+    }
+
     return(
         <>
-        <div className="bodyHome">
+        <div className={activeNight ? "bodyHome" : "bodyHomeNight"}>
             <div className="itemOne">
-                <div className="ballOne"></div>
-                <div className="ballSmallOne"></div>
+                <div className={activeNight ? "ballOne" : "ballOneNight"}></div>
+                <div className={activeNight ? "ballSmallOne" : "ballSmallOneNight"}></div>
+                <div className={activeNight ? "circleOne" : "circleOneNight"}></div>
             </div>
-            <div className="mainHome"></div>
+            <div className="mainHome">
+                <div className="container">
+                    <div className={activeNight ? "content" : "contentNight"}>
+                        <img src="../src/assets/cezar.png" alt="" />
+                        <h1>CEZAR RULL</h1>
+                        <ul>
+                            <li>
+                                <a href="#">CONTATO</a>
+                            </li>
+                            <li>
+                                <a href="#">SOBRE</a>
+                            </li>
+                            <li>
+                                <a href="#">OUTROS</a>
+                            </li>
+                        </ul>
+                        <button className={activeNight ? "buttonDay" : "buttonNight"} onClick={handleButtonNight}>
+                            <div className='sun'>
+                                <SunDim size={20} />
+                            </div>
+                            <div className='moon'>
+                                <MoonStars size={20} />
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
             <div className="itemTwo">
-                <div className="ballTwo"></div>
-                <div className="ballSmallTwo"></div>
+                <div className={activeNight ? "ballTwo" : "ballTwoNight"}></div>
+                <div className={activeNight ? "ballSmallTwo" : "ballSmallTwoNight"}></div>
+                <div className={activeNight ? "circleTwo" : "circleTwoNight"}></div>
             </div>
         </div>
         </>
